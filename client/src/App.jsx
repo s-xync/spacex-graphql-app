34b8bd56083ce8288e60a11fb1587ac3a1abada2
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-01-22 20:33:46 +0530

    completes with launch component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.png";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
 });
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img src={logo} className="spacex-logo" alt="SpaceX" />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <img src={logo} className="spacex-logo" alt="SpaceX" />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
